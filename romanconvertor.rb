284fed6ad9396c972f0c978eaf81510d254edfe6
dotty <dottyhorsman@yahoo.com>
2021-09-12 21:00:23 -0500

    33% tests working

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,33 @@
+# 1000 = M   
+# 500 = D    100 = C    
+# 50 = L     10 = X
+# 5 = V      4 = IV   1 = I
+
+
+# When "" * number, add number to string?
+
+
+
 def fromRoman(romanNumber)
     # Replace the following line with the actual code!
+    
     raise NotImplementedError
 end
 
 def toRoman(arabicNumber)
     # Replace the following line with the actual code!
-    raise NotImplementedError
+    converted = "";
+    roman_nums = Hash["M" => 1000, 
+        "D" => 500, "C" => 100, 
+        "L" => 50, "X" => 10, 
+        "V" => 5, "IV" => 4, "I" => 1];
+
+    if(arabicNumber < 1 || arabicNumber > 3999)
+        raise RangeError
+    end #not else?
+        roman_nums.each do |r, a|
+            converted << a * (arabicNumber / r); 
+            arabicNumber = arabicNumber % r;
+        end
+    converted; #like a return
 end
